ba99fdc7fe108b1ecaca9e3e2732f0f92e76b3cd
lsjv <lsjv1574@gmail.com>
2026-03-19 21:01:55 -0300

    fix api url

--- pulse_frontend/src/services/api.ts
+++ pulse_frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://pulse-jjzw.onrender.com';
+const API_BASE_URL = 'https://pulse-jjzw.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
